c75428b800d88f68e8551028a6069089fd27747a
BrunoY <51705553+BrunoY-OG@users.noreply.github.com>
2022-12-19 13:12:53 -0300

    parte1

--- EntregaAula05/Program.cs
+++ EntregaAula05/Program.cs
@@ -1,27 +1,76 @@
 ﻿using System;
+
 namespace Cs958 {
     public class exercicio1 {
-        private static void ColetaInput(string pergunta, out int valor) {
-            Console.Write($"{pergunta} : ");
-            while ((!int.TryParse(Console.ReadLine(), out valor))
-                    || (valor < 0)) {
-                Console.WriteLine("     Valor inválido.");
-                Console.Write("     Entre com um valor válido: ");
-            }
-        }
-
-        private int[,] ColetaMatrizQuadrada(int tamanho) {
-            int[,] result = new int[tamanho, tamanho];
-            for (int iC1 = 0; iC1 < tamanho; iC1++) {
-                for (int iC2 = iC1 + 1; iC2 < tamanho; iC2++) {
-                    ColetaInput($"Qual a distância de {iC1} para {iC2}?", out int distancia);
-                    result[iC1, iC2] = distancia;
-                    result[iC2, iC1] = distancia;
+        public static void Main() {
+            
+            bool ColetaLinha(out int[] listaNumeros) {
+                string? linhaLida = Console.ReadLine();
+                while ((linhaLida is null) || (linhaLida == string.Empty)) {
+                    Console.WriteLine($"Erro: linha vazia.");
+                    Console.Write($"Por favor, entre com a lista separada por ',':");
+                    linhaLida = Console.ReadLine();
                 }
+                string[] listaLinha = linhaLida.Split(",");
+                listaNumeros = new int[listaLinha.Length];
+                for (int iC1 = 0; iC1 < listaLinha.Length; iC1++) {
+                    if (!int.TryParse(listaLinha[iC1], out listaNumeros[iC1])) {
+                        Console.WriteLine($"Erro processando linha na coluna {iC1}:{Environment.NewLine}" +
+                                          $"    TryParse retornou false.");
+                        return false;
+                    }
+                }
+                return true;
             }
-            return result;
-        }
-        
 
+            bool ConverteuListaParaLinhadaMatriz(int[,] matriz, int currentLine, int[] listaNumeros) {
+                if (listaNumeros.Length > matriz.GetLength(currentLine)) {
+                    Console.WriteLine($"Erro processando matriz na linha {currentLine}:{Environment.NewLine}" +
+                                      $"    há mais elementos lidos do que a linha comporta.");
+                    return false;
+                }
+                for (int iC1 = currentLine + 1; iC1 < listaNumeros.Length; iC1++) {
+                    matriz[currentLine, iC1] = listaNumeros[iC1];
+                    matriz[iC1, currentLine] = listaNumeros[iC1];
+                }
+                return true;
+            }
+
+            int[,]? ColetaMatriz() {
+                Console.WriteLine("Por favor, entre com a matriz:");
+                if (!ColetaLinha(out var linhaLida)) return default;
+                int qntdCol = linhaLida.Length;
+                int[,] matriz = new int[qntdCol,qntdCol];
+                if (!ConverteuListaParaLinhadaMatriz(matriz, 0, linhaLida)) return default;
+                for (int iLinha = 1; iLinha < qntdCol; iLinha++) {
+                    if (!ColetaLinha(out linhaLida)) return default;
+                    if (!ConverteuListaParaLinhadaMatriz(matriz, iLinha, linhaLida)) return default;
+                }
+                return matriz;
+            }
+
+            int CalculaDistancia(int[,] distancias, int[] rota) {
+                var distanciaTotal = 0;
+                for (int cidade = 0; cidade < rota.Length - 1; cidade++) {
+                    if (rota[cidade + 1] >= distancias.GetLength(0)) {
+                        Console.WriteLine($"Erro processando a rota:{Environment.NewLine}" +
+                                          $"    A cidade número {rota[cidade + 1]} não existe.");
+                        return -1;
+                    }
+                    distanciaTotal += distancias[rota[cidade], rota[cidade + 1]];
+                }
+                return distanciaTotal;
+            }
+
+
+            int[,]? distancias = ColetaMatriz();
+            if (distancias is null) return;
+            Console.WriteLine("Entre com a rota:");
+            if (!ColetaLinha(out var rota)) return;
+            int totalDistancia = CalculaDistancia(distancias, rota);
+            if (totalDistancia >= 0) {
+                Console.WriteLine($"A distância total é de {totalDistancia}.");
+            }
+        }
     }
 }
